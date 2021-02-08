afb577426059e42b6a4b019e31c821bfb11d3ada
Simon Horrocks <S.M.Horrocks2@newcastle.ac.uk>
2021-02-08 20:01:02 +0000

    Added claim feature for claiming names, does not remember between boots currently

--- adjudicator.py
+++ adjudicator.py
@@ -1,14 +1,15 @@
 import os
 import random
+import re
 
 import discord
+from discord.ext import commands
 from dotenv import load_dotenv
 
 load_dotenv()
 TOKEN = os.getenv("DISCORD_TOKEN")
 GUILD = os.getenv("DISCORD_GUILD")
 DAD = os.getenv("DAD")
-
 
 welcome = [
     "You're welcome",
@@ -30,8 +31,19 @@ compliment = [
     "If you ever feel like the world is crashing down on you, just know I've got your back!"
 ]
 
-
 client = discord.Client()
+claim_regex = re.compile(r"(claim\s+(\w+))")
+claimed = dict()
+
+
+def claim(user_id, name):
+    if user_id in claimed.keys():
+        return 2
+    elif name in claimed.values():
+        return 0
+    else:
+        claimed[user_id] = name
+        return 1
 
 
 @client.event
@@ -47,11 +59,35 @@ async def on_ready():
 async def on_message(message):
     if message.author == client.user:
         return
-    elif message.author.name == DAD and message.content.lower().strip() in ["my son", "my son!"]:
+    elif (strip_message := message.content.strip()).startswith("!judy"):
+        if "claim" in strip_message:
+            matches = claim_regex.search(strip_message)
+            if matches:
+                if 1 == (claim_result := claim(message.author.id, matches[2])):
+                    await message.channel.send(f"claimed {matches[2]} for {message.author.name}")
+                elif 0 == claim_result:
+                    await message.channel.send(f"sorry {message.author.name}! {matches[2]} has already been claimed :(")
+                elif 2 == claim_result:
+                    await message.channel.send(f"sorry {message.author.name}! you've already claimed a name")
+            else:
+                await message.channel.send(f"sorry {message.author.name}! I'm not sure that's a valid name")
+        else:
+            await message.channel.send(f"sorry {message.author.name}! I didn't understand that")
+    elif (pro_message := message.content.lower().strip()) in ["my son", "my son!"] and message.author.name == DAD:
         response = random.choice(["Father!", "Where's Mother?"])
         await message.channel.send(response)
-    elif client.user in message.mentions and "thank" in message.content.lower().strip():
-        response = " ".join([random.choice(welcome), f"{message.author.name}.", random.choice(compliment)])
+    elif client.user in message.mentions:
+        if "thank" in pro_message:
+            response = " ".join([random.choice(welcome), f"{message.author.name}.", random.choice(compliment)])
+        elif "what" in pro_message:
+            response = """
+            I am Adjudicator, the arbiter of all things Bmiibo.
+            
+            In his spare time Father made a customizeable text-based autobattler that used Q-learning to attempt to learn 
+            how to fight. It was Father's intent to be able to play this with his friends (I'm guessing that's you) 
+            and since the game was text based he thought what better way to do it than with me, a discord bot!"""
+        else:
+            response = "How can I help?"
         await message.channel.send(response)
 
 
